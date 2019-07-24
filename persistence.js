b414814c11a3520c52f8dd909ca200940ac13fbf
freef <mtfreeland@gmail.com>
2019-07-24 15:53:10 -0600

    finished in kind of a bad way but meh

--- persistence.js
+++ persistence.js
@@ -1,6 +1,7 @@
-function persistence(num, per = 1) {
+function persistence(num, per = 0, bool = false) {
    let product = num.toString().split('').map(i => parseInt(i)).reduce((acc, val) => acc * val)
-   return (product < 10) ? per : persistence(product, per + 1)
+   return (product < 10) ? per + (bool=== false ? 0 : 1) : persistence(product, per + 1, true)
 }
 
+console.log(persistence(4))
 console.log(persistence(999))
