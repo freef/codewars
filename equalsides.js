571e3de643e173cbb0b9db0b5cafc48b9f70523a
freef <mtfreeland@gmail.com>
2019-07-25 13:49:17 -0600

    code wars problem to find point in array where both sides are equal. if there is no equal point then return -1

---
+++ equalsides.js
@@ -0,0 +1,8 @@
+function fei (arr) {
+  for (let i = 0; i < arr.length; i++) {
+  if (arr.slice(0, i).reduce(((acc,val) => acc + val),0) === arr.slice(i + 1).reduce(((acc,val)=> acc + val), 0)){
+    return i
+  }
+  }
+ return -1
+}
