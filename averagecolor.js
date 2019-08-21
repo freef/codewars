b3282738cec767716a55cd7007b5aa35b5aa5d51
freef <mtfreeland@gmail.com>
2019-08-21 10:00:36 -0600

    code challenge update comments

--- averagecolor.js
+++ averagecolor.js
@@ -1,12 +1,15 @@
 'use strict'
 
 const averageColor = (colorA, colorB) => {
-    // This helper function takes a hexidecimal string and splits it into an array of two letters. the letters are then converted to an integer.
+    // This helper function takes a hexidecimal string and splits it into an array of two letters.
+    // the letters are then converted to an integer.
     const toRGB = (hex) => (hex.match(/.{1,2}/g).map((element) => parseInt(element,16)))
-    // convert the two hexidecimal colors to RGB values
+    // call toRGB on the two hexidecimal colors to convert them to RGB values
     const a = toRGB(colorB)
     const b = toRGB(colorA)
-    // calculate the mean of each value. Math.floor ensures the retuned value is an integer.
+    // calculate the mean of each Red, Blue, and Green value.
+    // map with index is used to access the corresponding property in the second color
+    // Math.floor ensures the retuned value is an integer.
     // toString(16) converts the number to a hex
     // the array of hex values is then joined into a string
     const average = a.map((element, index) => (Math.floor((element +b[index]) /2)).toString(16)).join('')
@@ -15,12 +18,15 @@ const averageColor = (colorA, colorB) => {
 }
 
 // the function above returns the pure mathmatical average of the RGB integer values
-// however this approach will return a value that is sigificantly darker than the average of two color
+// however this approach will return a value that appears sigificantly darker than the expected average of two color
 // this is because when displaying an color on a monitor, all values will be squared prior to display
-// this function calculates the average color by squaring each value, then returns the square root of the average
+// and the average of two square roots is less than the square root of the average.
+// this function modifies the one above by calling Math.pow to square each value prior to averaging it
+// afterward this function takes the square root of the average
 // this method prevents the average color from appearing too dark
-const actualAverage =(colorA, colorB) => {
-  // This helper function takes a hexidecimal string and splits it into an array of two letters. the letters are then converted to an integer.
+const displayAverage =(colorA, colorB) => {
+    // This helper function takes a hexidecimal string and splits it into an array of two letters.
+    // the letters are then converted to an integer.
     const toRGB = (hex) => (hex.match(/.{1,2}/g).map((element) => parseInt(element,16)))
     // convert the hexidecimal colors to RGB values
     const a = toRGB(colorB)
