f062aeb23daa720fcc710c6ac658d6cb2d381927
freef <mtfreeland@gmail.com>
2020-09-08 14:25:04 -0400

    solution for converting to roman numeral

--- romannumerals.js
+++ romannumerals.js
@@ -2,18 +2,31 @@ let RomNum = class {
    
   constructor () {
     this.values = {
-    'I' : 1,
-    'V' : 5,
-    'X' : 10,
-    'L' : 50,
-    'C' : 100,
-    'D' : 500,
-    'M' : 1000
+      'M' : 1000,
+      'CM': 900,
+      'D' : 500,
+      'CD': 400,
+      'C' : 100,
+      'XC': 90,
+      'L' : 50,
+      'XL': 40,
+      'X' : 10,
+      'IX': 9,
+      'V' : 5,
+      'IV': 4,
+      'I' : 1 
   }
     }
   toRoman(i) {
-    
-    return i
+    let num = i
+    let str = ''
+    Object.entries(this.values).forEach(e => {
+      while (e[1] <= num) { 
+        str+=e[0]
+        num-=e[1]
+  }
+})
+    return str
   }
   fromRoman(j) {
     return j.split('').reduce((acc,val,i,arr) => Boolean(arr[i+1]) && this.values[arr[ i + 1]] > this.values[val] ? acc + (this.values[val] * -1) : acc + this.values[val], 0)
